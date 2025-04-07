89f5339034bc8bb7f228945dc1e6c16205be1c47
dejnah <168757297+dejnah@users.noreply.github.com>
2025-04-07 10:32:36 +0200

    L6 cvičení skripta kamen_nuzky_papir

--- kamen_nuzky_papir.py
+++ kamen_nuzky_papir.py
@@ -1,3 +1,5 @@
+#ZKONTROLUJ ZIP A  ZKUS SI TU LOGIKU VYMYSLET
+
 #naimportuj knihovnu random
 #zapiš si tyto proměnné
 #Nachystej smyčku while, která bude probíhat tak dlouho, dokud
@@ -8,6 +10,11 @@
 #5. Projdi současně jednotlivé volby jak v hrac_volby, tak pocitac_volby
 #6. pokud mají hrac i pocitac stejnou volbu (např. "kamen" a "kamen"), ulož do proměnné vysledek = "Remíza!"
 #7. pro všechny ostatní případy zapiš vysledek = "Vyhrává počítač!",
+#8. Úvodní rozhodovací podmínky máš napsané. Teď pojď doplnit zbytek a samozřejmě výstup.
+#9. dopiš logiku ve smyslu hry (např. hráč má "kamen", počítač "nuzky", tedy vyhrává hráč), kdy do proměnné vysledek = "Vyhrává hráč!",
+#10. zbytek podmínek máš zapsaných z předchozího kroku a proces je kompletní,
+#11. pro každý krok smyčky for ještě doplň výstup jako: "Výsledek: <VYSLEDEK>",
+#12. ukázkový výstup potom může vypadat následovně:
 
 import random
 
@@ -15,9 +22,22 @@ moznosti = ['kamen', 'nuzky', 'papir']
 hrac_volby = list()
 pocitac_volby = list()
 
-while len(hrac_volby) < 3:
+while len(hrac_volby) < 3 and len(pocitac_volby) < 3:
     hrac_volby.append(random.choice(moznosti))
-    while len(pocitac_volby) < 3:
-        pocitac_volby.append(random.choice(moznosti))
+    pocitac_volby.append(random.choice(moznosti))
+
 print(hrac_volby)
 print(pocitac_volby)
+
+for volba_hrac, volba_pocitac in zip(hrac_volby, pocitac_volby):
+    if volba_hrac == volba_pocitac:
+        vysledek = "Remíza!"
+    elif volba_hrac == "nuzky" and volba_pocitac == "kamen":
+        vysledek = "Vyhrává počítač!"
+    elif volba_hrac == "kamen" and volba_pocitac == "papir":
+        vysledek = "Vyhrává počítač!"
+    elif volba_hrac == "papir" and volba_pocitac == "nuzky":
+        vysledek = "Vyhrává počítač!"
+    else:
+        vysledek = "Vyhrává hráč!"
+    print(f"Výsledek: {vysledek}")
